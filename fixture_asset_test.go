fa49834e4e0a2a175d1f27701b576b0b91a83e27
MaximilianMeister <mmeister@suse.de>
2016-04-05 23:00:37 +0200

    extend coverage of code path for TotalCommission

--- fixture_asset_test.go
+++ fixture_asset_test.go
@@ -54,5 +54,6 @@ var orderTests = []struct {
 }{
 	{"consors", 1000, 3.56, 3.01, 2.87, 19.9, 332, 162.02, 67.06, 3.07, decimal.NewFromFloat(3.9)},
 	{"ingdiba", 2000, 38.56, 32.01, 29.87, 19.8, 62, 370.92, 167.86, 32.58, decimal.NewFromFloat(3.1)},
+	{"dab", 5000, 38.56, 32.01, 29.87, 37.42, 156, 977.94, 377.7, 32.29, decimal.NewFromFloat(3.1)},
 	{"comdirect", 29000, 385.06, 327.01, 298.87, 119.8, 89, 5150.54, 2520.37, 327.19, decimal.NewFromFloat(2.1)},
 }
